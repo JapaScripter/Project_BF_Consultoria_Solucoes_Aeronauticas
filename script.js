1c45c667e1dda7fbed3d8346d9ee4d13894b3748
JapaScripter <80179611+JapaScripter@users.noreply.github.com>
2024-06-27 19:37:09 -0300

    V.0.6

--- script.js
+++ script.js
@@ -30,21 +30,28 @@ botaoContato.addEventListener('click', function() {
 /*--|Header Scroll Button|--*/
 
 /*--|Mobile Menu|--*/
-function toggleMenu(){
+function toggleMenu() {
   var menuList = document.getElementById('lsm');
-  if (menuList.style.display ==='none' || menuList.style.display ===''){
-  menuList.style.display ='flex';}
-  else {
-    menuList.style.display ='none';
+  if (menuList.style.display === 'none' || menuList.style.display === '') {
+      menuList.style.display = 'flex';
+  } else {
+      menuList.style.display = 'none';
   }
 }
-function toggleoffMenu(){
+
+function toggleoffMenu() {
   var menuList = document.getElementById('lsm');
   var close = document.getElementById('cmn');
-  if (menuList.style.display ==='none' || menuList.style.display ===''){
-  menuList.style.display ='flex';}
-  else {
-    menuList.style.display ='none';
+  if (menuList.style.display === 'none' || menuList.style.display === '') {
+      menuList.style.display = 'flex';
+  } else {
+      menuList.style.display = 'none';
   }
 }
+
+// Adicione este código para garantir que o menu não seja exibido ao carregar a página
+document.addEventListener('DOMContentLoaded', function() {
+  var menuList = document.getElementById('lsm');
+  menuList.style.display = 'none'; // Inicialmente esconde o menu ao carregar a página
+});
 /*--|Mobile Menu|--*/
